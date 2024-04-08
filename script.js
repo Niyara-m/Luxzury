3e72098a40dfa6bd7a500a9b8c37fade1bacf048
Nika <nikam2341@gmail.com>
2024-04-08 11:16:10 +0500

    Add datepicker and turn on menu slider

--- script.js
+++ script.js
@@ -12,3 +12,35 @@ function closeMobileMenu() {
 	document.getElementById('closeIcon').style.display = 'none';
 }
 
+
+document.addEventListener('DOMContentLoaded', function() {
+    const menuItems = document.querySelectorAll('.menu-item');
+    const blocks = document.querySelectorAll('.blocks .item-block');
+
+    menuItems.forEach(function(menuItem, index) {
+        menuItem.addEventListener('click', function(event) {
+            event.preventDefault();
+            const target = this.getAttribute('href').substring(1);
+            blocks.forEach(function(block) {
+                if (block.id === target) {
+                    block.style.display = 'block';
+                } else {
+                    block.style.display = 'none';
+                }
+            });
+        });
+    });
+});
+
+function formatDate() {
+  let date = new Date();
+  let day = String(date.getDate()).padStart(2, '0');
+  let month = String(date.getMonth() + 1).padStart(2, '0');
+  let year = date.getFullYear();
+
+  return `${day}-${month}-${year}`;
+}
+
+document.querySelector('#myDatepicker').value = formatDate();
+document.querySelector('#myDatepicker2').value = formatDate();
+
